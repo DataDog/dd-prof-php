f2989ccde00b239e4dde5f41e826f73e2b1e0df9
r1viollet <74836499+r1viollet@users.noreply.github.com>
2022-02-16 22:37:35 +0100

    Cppcheck - use cmake's built in cpp check property (#25)
    * Cppcheck - use cmake's built in cpp check property
    As per morrisonlevi's proposal, avoid using manual lookup of files in favour of the cmake cpp check support
    All targets are now covered by cppcheck when the associated option is activated.
    To exclude zend related warnings, suppressions are generated using cmake variables pointing to include directories.

--- cmake/Modules/CppCheck.cmake
+++ cmake/Modules/CppCheck.cmake
@@ -1,31 +1,37 @@
 # Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
 # This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2022-Present Datadog, Inc.
-
+option(DATADOG_PHP_STATIC_ANALYSIS "Enable static analysis tooling" OFF)
 find_program(CPP_CHECK_COMMAND NAMES cppcheck)
-if (CPP_CHECK_COMMAND)
-#The manual : http://cppcheck.sourceforge.net/manual.pdf
+
+if (DATADOG_PHP_STATIC_ANALYSIS AND CPP_CHECK_COMMAND)
+
+  #The manual : http://cppcheck.sourceforge.net/manual.pdf
   message("-- CppCheck found : ${CPP_CHECK_COMMAND}")
 
-  # Listing all files to check manually (we could also use existing variables)
-  file(GLOB_RECURSE CPPCHECK_ALL_SOURCE_FILES profiling/*.c profiling/*.h components/*.c components/*.h)
   set(CPPCHECK_TEMPLATE "cppcheck:{id}:{file}:{line}:{severity}:{message}")
-
-  list(APPEND CPPCHECK_INCLUDE_CMD "-I${CMAKE_SOURCE_DIR}/profiling/")
-  list(APPEND CPPCHECK_INCLUDE_CMD "-I${CMAKE_SOURCE_DIR}")
 
   list(APPEND CPP_CHECK_COMMAND 
       "--enable=warning,performance,portability,information,style"
       "--template=${CPPCHECK_TEMPLATE}"
       "--quiet" 
-      "--suppressions-list=${CMAKE_SOURCE_DIR}/CppCheckSuppressions.txt"
-      ${CPPCHECK_INCLUDE_CMD}
+      "--suppress=missingInclude"
+      "--suppress=missingIncludeSystem"
+      "--suppress=unmatchedSuppression"
+      "--force"
+      "--error-exitcode=1"
       )
 
-  add_custom_target(
-      cppcheck
-      COMMAND ${CPP_CHECK_COMMAND}
-      --error-exitcode=1 # make sure CI pipeline fails
-      # --check-config #check what header files are missing
-      ${CPPCHECK_ALL_SOURCE_FILES}
-      )
+  # ignore all warnings coming from zend files
+  foreach(PhpConfig_Single_INCLUDE_PATH ${PhpConfig_INCLUDE_DIRS})
+    list(APPEND CPP_CHECK_COMMAND 
+        "--suppress=*:${PhpConfig_Single_INCLUDE_PATH}*")
+  endforeach()
+
+  # Let user define their own cpp check commands if needed.
+  if(NOT DEFINED CACHE{CMAKE_C_CPPCHECK})
+    set(CMAKE_C_CPPCHECK "${CPP_CHECK_COMMAND};--std=c11")
+  endif()
+  if(NOT DEFINED CACHE{CMAKE_CXX_CPPCHECK})
+    set(CMAKE_CXX_CPPCHECK "${CPP_CHECK_COMMAND};--std=c++11")
+  endif()
 endif()
